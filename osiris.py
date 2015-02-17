bf2fd781471775b6b2601c9edfb9f4a0921a0e85
Sean <sean@tox.im>
2015-02-17 15:43:46 -0800

    Initial language detection

---
+++ osiris.py
@@ -0,0 +1,21 @@
+import json, glob
+
+def runonce():
+    lang_json = {}
+    lang_dir = "app/download/*.json"
+
+    for lang in glob.glob(lang_dir):
+        lang_code = lang.split('.')[1]
+        file_obj = open("app/download/index." + lang_code + ".json")
+        lang_json[lang_code] = json.loads(file_obj.read())
+        file_obj.close()
+
+    return lang_json
+
+def reply(msg):
+    try:
+        lang = msg['header']['accept-language'].split(',')[0].split('-')[0]
+    except:
+        lang = 'en'
+
+    return { "code": 200, "file": "index.mustache", "template": msg['runonce'][lang] }
